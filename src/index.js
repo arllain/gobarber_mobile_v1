b502893ff98d1d88ccb2683fce6b06499dea5a43
arllainm <arllain@gmail.com>
2020-04-05 19:33:58 -0300

    Redux config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 require('react-native').unstable_enableLogBox();
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 import '~/config/ReactotronConfig';
 import Routes from '~/routes';
 import { StatusBar } from 'react-native';
-
+import { store, persistor } from '~/store';
 export default function src() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }

4264b8f348279741d708eece7c2005f5c42dc425
arllainm <arllain@gmail.com>
2020-04-05 17:51:53 -0300

    Reactotron-Redux & Reactotron-Saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,8 +1,14 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronReduxSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
-  const tron = Reactotron.configure().useReactNative().connect();
+  const tron = Reactotron.configure({ host: '192.168.25.168' })
+    .useReactNative()
+    .use(reactotronRedux())
+    .use(reactotronReduxSaga())
+    .connect();
 
-  console.tron = tron;
   tron.clear();
+  console.tron = tron;
 }
